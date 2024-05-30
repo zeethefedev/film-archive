d5a8ab41c43d1ae38a3e6a73c3845fffe7d2065c
zeethefedev <zeethefrontenddev@gmail.com>
2024-05-30 17:17:35 +0700

    change favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
+import Head from "next/head";
 
 const poppins = Poppins({
   weight: ["400", "500", "600"],
@@ -10,6 +11,9 @@ const poppins = Poppins({
 export const metadata = {
   title: "The Silence Archive",
   description: "Film is not dead",
+  icons: {
+    icon: "/film-roll.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
